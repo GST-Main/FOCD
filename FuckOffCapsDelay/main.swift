1effc0dbd6c522eaa112f9eb34e86d85e579313b
GST <gst.soft.65536@gmail.com>
2024-11-23 14:42:54 +0900

    Update Logger

--- FuckOffCapsDelay/main.swift
+++ FuckOffCapsDelay/main.swift
@@ -3,7 +3,7 @@ import os
 
 let app = NSApplication.shared
 let delegate = AppDelegate()
-let logger = Logger()
+let logger = Logger(subsystem: "FOCD", category: "")
 app.delegate = delegate
 app.setActivationPolicy(.accessory)
 _ = NSApplicationMain(CommandLine.argc, CommandLine.unsafeArgv)
